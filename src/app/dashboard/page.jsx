8503e68b5d19ba7a68f5d466cb58e56eabd2899e
Vaibhav Katariya <86615124+VaibhavKatariya@users.noreply.github.com>
2025-02-23 01:28:13 +0530

    refactor: seprate dashboard

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,48 +1,10 @@
-'use client'
-import { useAuthState } from "react-firebase-hooks/auth";
-import { useEffect, useState } from "react";
-import { useRouter } from "next/navigation";
-import { auth } from "@/lib/firebase"; // Ensure this path matches your Firebase config
-import { AppSidebar } from "@/components/App-sidebar";
-import { Separator } from "@/components/ui/separator";
-import {
-  SidebarInset,
-  SidebarProvider,
-  SidebarTrigger,
-} from "@/components/ui/sidebar";
-import SponsorEmailDashboard from "@/components/email";
+import React from 'react'
+import NormalDash from './normalDash'
 
-export default function Home() {
-  const [user, loading] = useAuthState(auth);
-  const router = useRouter();
-  const [checkingAuth, setCheckingAuth] = useState(true);
-
-  useEffect(() => {
-    if (!loading) {
-      if (!user) {
-        router.push("/");
-      } else {
-        setCheckingAuth(false);
-      }
-    }
-  }, [user, loading, router]);
-
-  if (checkingAuth) {
-    return <div className="flex justify-center items-center h-screen">Loading...</div>;
-  }
-
+const page = () => {
   return (
-    <SidebarProvider>
-      <AppSidebar />
-      <SidebarInset>
-        <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
-          <SidebarTrigger className="-ml-1" />
-          <Separator orientation="vertical" className="mr-2 h-4" />
-        </header>
-        <main>
-          <SponsorEmailDashboard />
-        </main>
-      </SidebarInset>
-    </SidebarProvider>
-  );
+    <NormalDash/>
+  )
 }
+
+export default page
