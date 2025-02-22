d3db4f3c331397f11fe890186ff4694a99db44c5
Vaibhav Katariya <86615124+VaibhavKatariya@users.noreply.github.com>
2025-02-23 03:34:43 +0530

    feat: set dark mode as default for /u layout

--- src/app/u/layout.js
+++ src/app/u/layout.js
@@ -1,7 +1,7 @@
 'use client'
+
 import { AppSidebar } from "@/components/App-sidebar";
 import { Separator } from "@/components/ui/separator";
-
 import {
     SidebarInset,
     SidebarProvider,
@@ -10,17 +10,19 @@ import {
 
 export default function RootLayout({ children }) {
     return (
-        <SidebarProvider>
-            <AppSidebar />
-            <SidebarInset>
-                <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
-                    <SidebarTrigger className="-ml-1" />
-                    <Separator orientation="vertical" className="mr-2 h-4" />
-                </header>
-                <main>
-                    {children}
-                </main>
-            </SidebarInset>
-        </SidebarProvider>
+        <div className="dark">
+            <SidebarProvider>
+                <AppSidebar />
+                <SidebarInset>
+                    <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
+                        <SidebarTrigger className="-ml-1" />
+                        <Separator orientation="vertical" className="mr-2 h-4" />
+                    </header>
+                    <main>
+                        {children}
+                    </main>
+                </SidebarInset>
+            </SidebarProvider>
+        </div>
     );
 }
