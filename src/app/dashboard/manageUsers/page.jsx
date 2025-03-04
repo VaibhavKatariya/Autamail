1c302da201445dc08e460e09eecf82a88c078941
Vaibhav Katariya <86615124+VaibhavKatariya@users.noreply.github.com>
2025-03-05 01:45:14 +0530

    style: change layout of manageuser page

--- src/app/dashboard/manageUsers/page.jsx
+++ src/app/dashboard/manageUsers/page.jsx
@@ -18,9 +18,9 @@ export default function UsersPage() {
 
   return (
     <div className="flex flex-col items-center justify-start p-4 space-y-4">
-      <AdminDashboard />
       <ApproveUsers />
       <UsersList />
+      {/* <AdminDashboard /> */}
     </div>
   );
 }
