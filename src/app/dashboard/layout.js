fe73514444443992de65e8cffd2c0d0c4861f35f
Vaibhav Katariya <86615124+VaibhavKatariya@users.noreply.github.com>
2025-03-22 12:18:06 +0530

    feat: add Maintenance Mode

--- src/app/dashboard/layout.js
+++ src/app/dashboard/layout.js
@@ -1,4 +1,4 @@
-'use client';
+"use client";
 
 import { AppSidebar } from "@/components/sidebar";
 import SendEmailFormSkeleton from "@/components/skeletonUI/sendEmailForm";
@@ -8,30 +8,45 @@ import {
     SidebarProvider,
     SidebarTrigger,
 } from "@/components/ui/sidebar";
-import { useAuth } from "@/context/AuthContext"; // Import useAuth
+import { useAuth } from "@/context/AuthContext";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 
 export default function DashboardLayout({ children }) {
-    const { user, loading, checkingAuth } = useAuth(); // Use the hook
+    const { user, loading, checkingAuth } = useAuth();
     const router = useRouter();
+
+    const isMaintenanceMode = process.env.NEXT_PUBLIC_MAINTENANCE_MODE === "true";
 
     useEffect(() => {
         // Redirect to home if not authenticated
         if (!loading && !checkingAuth && !user) {
             router.replace("/");
-            return
+            return;
         }
     }, [user, loading, checkingAuth, router]);
 
-    // Show loading state while checking authentication or user role
+    // Show loading state while checking authentication
     if (loading || checkingAuth) {
         return <SendEmailFormSkeleton />;
     }
 
-    if (!user) return null; // Redirect to home if not authenticated
+    // Redirect to home if not authenticated (handled by useEffect, but added as a fallback)
+    if (!user) return null;
 
-    // Render the dashboard layout if authenticated
+    // Show maintenance page if in maintenance mode
+    if (isMaintenanceMode) {
+        return (
+            <div className="dark:text-white flex flex-col items-center justify-center w-full h-screen p-4">
+                <h1 className="text-3xl font-bold mb-4">Maintenance in Progress</h1>
+                <p className="text-lg text-center">
+                    The app is currently unavailable due to scheduled maintenance. Please try again later.
+                </p>
+            </div>
+        );
+    }
+
+    // Render the dashboard layout if authenticated and not in maintenance mode
     return (
         <div className="dark text-white">
             <SidebarProvider>
@@ -41,9 +56,7 @@ export default function DashboardLayout({ children }) {
                         <SidebarTrigger className="-ml-1" />
                         <Separator orientation="vertical" className="mr-2 h-4" />
                     </header>
-                    <main>
-                        {children}
-                    </main>
+                    <main>{children}</main>
                 </SidebarInset>
             </SidebarProvider>
         </div>
